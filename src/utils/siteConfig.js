bf3fe06d6401be019bf2031e847859521c981048
Juuro <mail@sebastian-engel.de>
2020-05-29 17:50:36 +0200

    Change site url.

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://naughty-euclid-aa5edb.netlify.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://trailstories.net`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
